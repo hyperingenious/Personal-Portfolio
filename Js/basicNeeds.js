2e6c5fe78073d76709e5c319e77c7df4ad3733b0
Saurav Verma <88635129+SauravIsAlive@users.noreply.github.com>
2022-09-11 00:11:20 +0530

    cards

---
+++ Js/basicNeeds.js
@@ -0,0 +1,4 @@
+const projectQuantity = document.querySelector('.swiper-head')
+allSwiperSlides = document.querySelectorAll('.swiper-slide')
+
+projectQuantity.innerHTML = `${allSwiperSlides.length}+`
