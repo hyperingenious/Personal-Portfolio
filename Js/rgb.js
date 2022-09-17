efa4d3dcb8fe56e09f955042985d8fa4a59ff05e
Saurav Verma <88635129+SauravIsAlive@users.noreply.github.com>
2022-09-17 13:52:48 +0530

    workig ``

--- Js/rgb.js
+++ Js/rgb.js
@@ -1,57 +1,49 @@
-/*
+const rgbClass = document.querySelectorAll('.rgb')
 let colorValue = 0;
+
+
+const clickButton = document.querySelector('.preRgb');
+let colorValue2 = 0;
+
+
 setInterval(() => {
-
-    //Array Of Html Classes
-    const rgbClasses = [
-        document.querySelectorAll('.px-11'),
-        document.querySelectorAll('.spanTag'),
-        document.querySelectorAll('h1'),
-        document.querySelectorAll('.bigSpan'),
-        document.querySelectorAll('path'),
-        document.querySelectorAll('polyline'),
-        document.querySelectorAll('.skill_box')
-    ]
-
-    //Variables for HTML classes
-    const [allLi, allSpan, allH1, allBigSpan, allPath, allPolyline, allSkillBox] = rgbClasses;
-
-
-    if (colorValue <= 360) {
-        colorValue++
-        // document.querySelector('#logo').style.color = `hsl(${colorValue}, 100%, 57%)`;
-
-
-        allLi.forEach((e) => {
-            e.style.color = `hsl(${colorValue},100%,30%)`;
-        })
-        allSpan.forEach((e) => {
-            e.style.backgroundColor = `hsl(${colorValue},100%,30%)`;
-        })
-        allH1.forEach((e) => {
-            e.style.color = `hsl(${colorValue},100%,30%)`;
-        })
-        allBigSpan.forEach((e) => {
-            e.style.backgroundColor = `white`;
-        })
-        allPath.forEach((e) => {
-            e.style.stroke = `hsl(${colorValue},100%,30%)`;
-        })
-        allPolyline.forEach((e) => {
-            e.style.stroke = `hsl(${colorValue},100%,30%)`;
-        })
-        allSkillBox.forEach((e) => {
-            e.style.borderColor = `hsl(${colorValue},100%,30%)`;
-        })
-
+    if (colorValue2 <= 360) {
+        colorValue2++;
+        clickButton.style.color = `hsl(${colorValue2},100%,50%)`
     }
-    if (colorValue >= 360) {
-        colorValue = 0
-    }
+    else if (colorValue2 >= 360) { colorValue2 = 0; }
 }, 10);
 
+const rgbFunction = function () {
 
-*/
+    if (colorValue == 0) {
+        setInterval(() => {
+            //getting an rgb class
+            if (colorValue <= 360) {
+                colorValue++
+                rgbClass.forEach((e) => {
+                    e.style.backgroundColor = `hsl(${colorValue},100%,50%)`;
+                })
+            }
+            else if (colorValue >= 360) {
+                colorValue = 0
+            }
+
+        }, 10)
+    }
+    else if(colorValue>0) {
+        rgbClass.forEach((e) => {
+            e.style.backgroundColor = '#00FF85';
+        })
+    }
+}
+clickButton.addEventListener('click', rgbFunction)
+
+
+
+
+
+
 /*
 let colorValue = 0;
 setInterval(() => {
